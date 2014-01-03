2117ec72fa57ce7b0eceadd2a7fcd89dfd4f2779
Luke Sullivan <luke_sullivan@harvard.edu>
2014-01-03 13:21:21 -0500

    built InfobloxConn class with getfixedaddr method, class reads config file for IB connection details

--- ib-query.py
+++ ib-query.py
@@ -3,22 +3,39 @@ import sys
 import json
 import requests
 import pprint
+import ConfigParser
 
-ADDRESS = '128.103.200.106'
-USERNAME = 'api-ro'
-PASSWORD = '35LuWKlZnTKbKRbevraK'
-PATH = '/wapi/v'
-VERIFY = False
-VERSION = '1.2'
+class InfobloxConn:
+    def __init__(self, conffile):
+        config = ConfigParser.ConfigParser()
+        try:
+            config.read(conffile)
+        except:
+            print "failed opening config file, giving up"
+            sys.exit(0)
+        try:
+            self.ib_address = config.get('IB', 'address')
+            self.username = config.get('IB', 'username')
+            self.path = config.get('IB', 'path')
+            self.password = config.get('IB', 'password')
+            self.version = config.get('IB', 'version')
+            if config.get('IB', 'verify') == 'True':
+                self.verify = True
+            else:
+                self.verify = False
+        except:
+            print "conf file appears to be corrupt or does not contain all needed values to set up IB connection"
+            sys.exit(0)
 
-def make_req():
-    r = requests.get('https://' + ADDRESS + PATH + VERSION + '/fixedaddress?ipv4addr~=128.103&_return_fields=ipv4addr,network,mac&_max_results=-10000', auth=(USERNAME, PASSWORD), verify=VERIFY)
-    foo = r.json()
-    for i in foo:
-        print i
+    def getfixedaddr(self, addr, fields, numresults):
+        r = requests.get('https://' + self.ib_address + self.path + self.version + '/fixedaddress?ipv4addr~=' + addr + '&_return_fields=' + fields + '&_max_results=-' + str(numresults), auth=(self.username, self.password), verify=self.verify)
+        return r.json()
 
 def main():
-    make_req()
+    a = InfobloxConn('ib.cfg')
+    r = a.getfixedaddr('128.103.209', 'ipv4addr,network,mac', 10000)
+    for i in r:
+        print i
 
 if __name__ == '__main__':
     sys.exit(main())
