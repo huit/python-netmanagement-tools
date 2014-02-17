578f73d1053a5dadce138ffb5e46eaa63f9108df
Luke Sullivan <luke_sullivan@harvard.edu>
2014-02-16 23:09:51 -0500

    new methods added for getting dns zone data from ib

--- ibquery.py
+++ ibquery.py
@@ -26,10 +26,29 @@ class InfobloxConn:
             print "conf file appears to be corrupt or does not contain all needed values to set up IB connection"
             sys.exit(0)
 
-    def getfixedaddr(self, addr, fields, numresults):
+    def getfixedaddr(self, ipaddr, fields, numresults):
 
         # gets all fixed addresses in a given network range
 
-        r = requests.get('https://' + self.ib_address + self.path + self.version + '/fixedaddress?ipv4addr~=' + addr + '&_return_fields=' + fields + '&_max_results=-' + str(numresults), auth=(self.username, self.password), verify=self.verify)
+        r = requests.get('https://' + self.ib_address + self.path + self.version + '/fixedaddress?ipv4addr~=' + ipaddr + '&_return_fields=' + fields + '&_max_results=-' + str(numresults), auth=(self.username, self.password), verify=self.verify)
         return r.json()
 
+    def getmacfilteraddress(self, macaddr, fields, numresults):
+
+        r = requests.get('https://' + self.ib_address + self.path + self.version + '/macfilteraddress?mac=' + macaddr + '&_return_fields=' + fields + '&_max_results=-' + str(numresults), auth=(self.username, self.password), verify=self.verify)
+        return r.json()
+
+    def getleasebyip(self, ipaddr, fields, numresults):
+
+        r = requests.get('https://' + self.ib_address + self.path + self.version + '/lease?address=' + ipaddr + '&_return_fields=' + fields + '&_max_results=-' + str(numresults), auth=(self.username, self.password), verify=self.verify)
+        return r.json()
+
+    def getauthzone(self, zonename, fields):
+
+        r = requests.get('https://' + self.ib_address + self.path + self.version + '/zone_auth?fqdn=' + zonename + '&_return_fields=' + fields, auth=(self.username, self.password), verify=self.verify)
+        return r.json()
+
+    def getdelegatedzone(self, zonename, fields):
+
+        r = requests.get('https://' + self.ib_address + self.path + self.version + '/zone_delegated?fqdn=' + zonename + '&_return_fields=' + fields, auth=(self.username, self.password), verify=self.verify)
+        return r.json()
